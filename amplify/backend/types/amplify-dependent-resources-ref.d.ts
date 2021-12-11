eaffab758d7e025589f85e4dde258080569556c5
Melissa McGuire <74629223+maggiemadison@users.noreply.github.com>
2021-12-11 13:09:45 -0600

    create-react-app, npm install aws-amplify @aws-amplify/ui-react@1.2.22, amplify init, amplify add auth, amplify add hosting, imports @src/index.js & App.js, amplify publish, amplify delete, amplify init & add auth & push

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "fullstackappb72c3db7": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
