52487853435d343b6b9c12d34e6f4a397636da82
Melissa McGuire <74629223+maggiemadison@users.noreply.github.com>
2021-12-11 13:52:10 -0600

    updates to App.js

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello World!</h1>
+        <h2>Project 6</h2>
+        <h3>Advanced Cloud Fall 2021</h3>
       </header>
     </div>
   );
